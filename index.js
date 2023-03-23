ae09e191ad04c2842fc5031f7c3adc1c49a75638
Ernest <ernest.okiror@student.moringaschool.com>
2023-03-23 15:12:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickuplocationofpassenger){
+    let Scuberheadquaters = 42
+    return Math.abs(pickuplocationofpassenger - Scuberheadquaters)
+}
+function distanceFromHqInFeet(pickupLocation) {
+    let feetPerBlock = 264;
+    const blocksAway = distanceFromHqInBlocks(pickupLocation);
+    return blocksAway * feetPerBlock;
+  } 
+    //returns the number of feet traveled
+function distanceTravelledInFeet(start, destination){
+    let feetPerBlock = 264
+    let distanceinfeet = Math.abs(start - destination)
+    return(distanceinfeet * feetPerBlock)
+
+}
+function  calculatesFarePrice(start, destination) {
+    let freesample= distanceTravelledInFeet(start, destination)
+    if(freesample<= 400){
+    return 0
+    }
+      else if (freesample<=2000){
+          const fare=(freesample-400)*0.02
+          return fare
+    }
+       else if (freesample>2000 && freesample<2500 ){
+          const charge =25
+          return charge
+       }
+       else if (freesample>2500){
+          const reject = 'cannot travel that far'
+          return reject
+       }
+}
+        
+ 
+  
+      
+
+
+
+     
+    
+    
